d8bd07f69ab41a2e39ccfe4b64bd2710b31ab82c
Ana Gil <agil@cpuras-iMac.local>
2018-08-31 14:42:33 +0200

    Making columna Name fillable

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Profession extends Model
-{
-    //
+{   
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
 }
